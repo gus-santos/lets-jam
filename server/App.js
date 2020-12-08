7b434def64e293a632511156c7157db20e17d8a5
Gus Santos <gsantos@Guss-MacBook-Pro.local>
2020-12-08 09:35:07 -0800

    Remove messaging from App.js for now

--- server/App.js
+++ server/App.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 const visitorRoute = require('./routes/visitor');
 const mentalHealthRoute = require('./routes/mental-health');
 const postsRoute = require('./routes/posts');
-const messagingRoute = require('./routes/messaging');
+// const messagingRoute = require('./routes/messaging');
 
 // Middleware
 app.use(express.json());
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use('/', visitorRoute);
 app.use('/mental-health', mentalHealthRoute);
 app.use('/:id', postsRoute); // Let's hope this works without the need for a userRoute (OPTIONAL if id === loggedUserId show own profile with edit options)
-app.use('/messaging', messagingRoute);
+// app.use('/messaging', messagingRoute);
 
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
