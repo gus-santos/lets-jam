024d26ded94b590e31d5d4ae1c6e045e36a76a4a
gus-santos <72359205+gus-santos@users.noreply.github.com>
2020-12-10 01:18:01 -0800

    Revert "Features"

--- server/bookshelf.js
+++
@@ -1,7 +0,0 @@
-const knex = 
-    process.env.NODE_ENV === 'production'
-        ? require('knex')(require('./knexfile').production)
-        : require('knex')(require('./knexfile').development);
-const bookshelf = require('bookshelf')(knex);
-
-module.exports = bookshelf;
