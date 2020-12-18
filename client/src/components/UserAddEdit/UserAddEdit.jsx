009714671981adfdd74e93a239eb9b60c31f293e
Gus Santos <gsantos@Guss-MacBook-Pro.local>
2020-12-17 21:02:18 -0800

    Testing

--- client/src/components/UserAddEdit/UserAddEdit.jsx
+++ client/src/components/UserAddEdit/UserAddEdit.jsx
@@ -193,76 +193,12 @@ class UserAddEdit extends React.Component {
                         />
                     </label>
                     <label className="user-add-edit__label field-row-stacked">
-                        Phone:&nbsp;
-                        <input
-                            type="text"
-                            name="phone"
-                            // autoComplete="off"
-                            defaultValue={this.state.contactInfo.phone}
-                            onChange={this.updatePhone} // [Q] Can I add the existing validation here, too?
-                        />
-                    </label>
-                    <label className="user-add-edit__label field-row-stacked">
-                        SoundCloud:&nbsp;
-                        <input
-                            type="text"
-                            name="soundcloud"
-                            defaultValue={this.state.contactInfo.soundcloud}
-                            onChange={this.updateSoundcloud}
-                        />
-                    </label>
-                    <label className="user-add-edit__label field-row-stacked">
-                        Bandcamp:&nbsp;
-                        <input
-                            type="text"
-                            name="bandcamp"
-                            defaultValue={this.state.contactInfo.bandcamp}
-                            onChange={this.updateBandcamp}
-                        />
-                    </label>
-                    <label className="user-add-edit__label field-row-stacked">
-                        Facebook:&nbsp;
-                        <input
-                            type="text"
-                            name="facebook"
-                            defaultValue={this.state.contactInfo.facebook}
-                            onChange={this.updateFacebook}
-                        />
-                    </label>
-                    <label className="user-add-edit__label field-row-stacked">
-                        Website:&nbsp;
-                        <input
-                            type="text"
-                            name="website"
-                            defaultValue={this.state.contactInfo.website}
-                            onChange={this.updateWebsite}
-                        />
-                    </label>
-                    <label className="user-add-edit__label field-row-stacked">
                         User name:&nbsp;
                         <input
                             type="text"
                             name="screenName" // [Q] How can I prevent users from using anything other than lowercase and underscore? Ties in with other question
                             defaultValue={this.state.screenName}
                             onChange={this.updateUserName}
-                        />
-                    </label>
-                    <label className="user-add-edit__label field-row-stacked">
-                        Type:&nbsp;
-                        <input
-                            type="text"
-                            name="type"
-                            defaultValue={this.state.type}
-                            onChange={this.updateType}
-                        />
-                    </label>
-                    <label className="user-add-edit__label field-row-stacked">
-                        Show mental health settings:&nbsp;
-                        <input
-                            type="checkbox"
-                            name="healthSettings"
-                            value="1"
-                            onChange={this.updateHealthSettings}
                         />
                     </label>
                     <label className="user-add-edit__label field-row-stacked">
@@ -302,30 +238,12 @@ class UserAddEdit extends React.Component {
                         />
                     </label>
                     <label className="user-add-edit__label field-row-stacked">
-                        Favourite studio:&nbsp;
-                        <input
-                            type="text"
-                            name="favouriteStudio"
-                            defaultValue={this.state.favouriteStudio}
-                            onChange={this.updateFavouriteStudio}
-                        />
-                    </label>
-                    <label className="user-add-edit__label field-row-stacked">
                         Skills:&nbsp;
                         <input
                             type="text"
                             name="skills"
                             defaultValue={this.state.skills}
                             onChange={this.updateSkills}
-                        />
-                    </label>
-                    <label className="user-add-edit__label field-row-stacked">
-                        Instruments:&nbsp;
-                        <input
-                            type="text"
-                            name="instruments"
-                            defaultValue={this.state.instruments}
-                            onChange={this.updateInstruments}
                         />
                     </label>
                     <label className="user-add-edit__label field-row-stacked">
