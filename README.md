978bd58e5122528b2a1365c52c2f071fa3c99c9c
Gus <santos.gus@protonmail.com>
2021-05-10 14:07:04 -0700

    Add proper readme file

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# Let's Jam!
+# Let's Jam
+
+Let's Jam is a social network (it's actually more of a forum, but the kids might not know what a forum is anymore) that hopes to connect musicians based in British Columbia's Lower Mainland.
+
+The tech stack is: Node.js, React.js, Express, MySQL, Knex.js, Bookshelf.js
+
+## Setup
+
+### 1. General
+
+`npm run setup:dev` installs all npm packages needed for both client and server sides.
+
+### 2. Database
+
+*/server/knexfile.js* contains MySQL parameters. The default schema name is *lets-jam*, with the _root_ user taking _rootroot_ as the password.
+
+After setting up the schema, `npm run migrate:seed` seeds the database with dummy data.
+
+### 3. Client and server
+
+`npm run start` runs the server and the client concurrently.
+
+## What doesn't work yet
+
+- Search
+
+## What I still hope to implement
+
+- Responsive design
+- Image upload
+- Video upload
+- Multiple users
+- Likes
+- Befriend
+- Google Distance Matrix API to show distance between users
+- Form validation
+- Search
+- BSOD as modal
+- Settings
+- FAQ
+- About
--- client/src/pages/Visitor/Visitor.jsx
+++ client/src/pages/Visitor/Visitor.jsx
@@ -10,12 +10,13 @@ const Visitor = () => {
             <p className="visitor__text">Social isolation is a common complaint in our region, with 41% of British Columbians <a href="https://vancouversun.com/sponsored/news-sponsored/united-way-of-the-lower-mainland-is-tackling-loneliness-and-social-isolation-head-on/" className="visitor__link">reporting</a> not talking to many of their neighbours.</p>
             <p className="visitor__text">Proportionally, more musicians struggle with <a href="https://consequenceofsound.net/2019/05/73-percent-musicians-mental-health-illness-depression-report/" className="visitor__link">mental illness</a> than the general population, and most of us make our best music by playing with other people.</p>
             <p className="visitor__text">Once these crazy times are over, Let's Jam!</p>
+            
+            {/* Buttons */}
             <a href="/add-user" className="visitor__button--link">
                 <Button type="submit" label="Join now" />
             </a>
-            
             <a href="/login" className="visitor__button--link">
-            <Button type="submit" label="Log in" />
+                <Button type="submit" label="Log in" />
             </a>
         </pre>
     );
